99c94a518fb9cd34bb6591b6d402676d56fcde17
lengocloi2003 <62826388+lengocloi2003@users.noreply.github.com>
2021-09-27 00:09:56 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,18 +1,24 @@
-def even_number(func):
-    A = list(map(int, input().split()))
-    def count_number():
-        count = 0
-        for x in A:
-            if x % 2 == 0:
-                count+=1
-        return count
-    if count_number()>10:
-        func()
-    elif count_number() == 0 :
-        print('Нет')
-@even_number
-def print_count():
-    print("Очень много")
+def decorator(func):
+    def wrapper(*args,**kwargs):
+        val = func(*args,**kwargs)
+        if val == 0 :
+            return 'Нет'
+        elif val > 10:
+            return 'очень много'
+        else:
+            return val
+    return wrapper
+
+@decorator
+def count_even_number(A):
+    count = 0
+    for x in A:
+        if x % 2 == 0:
+            count += 1
+    return count
+
+A = list(map(int,input().split()))
+print(count_even_number(A))
 
 
 
