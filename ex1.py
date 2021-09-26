ba5804acdf649583b3d29af063640a4b591fcb02
lengocloi2003 <62826388+lengocloi2003@users.noreply.github.com>
2021-09-26 23:11:24 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,9 +1,11 @@
+# Напишите консольную программу, которой на вход подается единственное число N (без имени или с именем -n),
+# а программа печатает значение Nго числа Фибоначчи
 import sys
 import argparse
 
 def createParser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument ('-n','--name')
+    parser.add_argument ('-n')
 
     return parser
 
@@ -17,4 +19,4 @@ if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args()
 
-    print (fibo(int(namespace.name)))
+    print (fibo(int(namespace.n)))
